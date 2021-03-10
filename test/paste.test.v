74505031568704402f8c0c5bc9ba48a6e66d9e38
billyeatcookies <billydevbusiness@gmail.com>
2021-03-10 16:24:44 +0530

    Endpoints and ypes moved to the main directory

--- test/paste.test.v
+++ test/paste.test.v
@@ -5,9 +5,11 @@ module pastemyst
 // get public paste
 // println("Getting paste")
 // println(get_paste(id: '99is6n23'))
+
 // get private paste
 // println("Getting private paste")
 // println(get_paste(id: 'xc9mvyaq', token: 'token'))
+
 // create public paste 
 
 // fn main () {
@@ -24,3 +26,23 @@ module pastemyst
 //     mut result := create_paste(paste: new_paste) ?
 //     print(result.str())
 // }
+
+// create private paste 
+
+// fn main () {
+//     mut new_pasty := Pasty {
+//         language: "autodetect"
+//         title   : "test"
+//         code    : "print('test')"
+//     }
+//     mut new_paste := Paste {
+//         title   : "test"
+//         pasties : [new_pasty]
+//     }
+
+//     mut result := create_paste(paste: new_paste, token: "token") ?
+//     print(result.str())
+// }
+
+// delete a paste
+
