189e3f58345db0aec47cc0376530d63d5107ebab
billyeatcookies <billydevbusiness@gmail.com>
2021-03-11 14:10:38 +0530

    Imports from parent module

--- endpoints/data.v
+++ endpoints/data.v
@@ -2,8 +2,8 @@ module endpoints
 
 import billyeatcookies.pastemyst
 
-const data_language_endpoint_name           = "$main_endpoint/data/language"
-const data_language_endpoint_extension      = "$main_endpoint/data/languageExt"
+const data_language_endpoint_name           = "$pastemyst.main_endpoint/data/language"
+const data_language_endpoint_extension      = "$pastemyst.main_endpoint/data/languageExt"
 
 
 pub struct GetLanguageConfig {
@@ -13,13 +13,13 @@ pub struct GetLanguageConfig {
 
 pub fn get_language (config GetLanguageConfig) ?RawLanguage {
 	if config.name != "" {
-		mut request := http.new_request(.get, data_language_endpoint_name + "?name=" + config.name, "") ?
+		mut request := pastemyst.http.new_request(.get, data_language_endpoint_name + "?name=" + config.name, "") ?
 	} else if config.extension != "" {
-		mut request := http.new_request(.get, data_language_endpoint_extension + "?extension=" + config.extension, "") ?
+		mut request := pastemyst.http.new_request(.get, data_language_endpoint_extension + "?extension=" + config.extension, "") ?
 	}
 	response := request.do() ?
-	if response.status_code == http.Status.ok {
-		return json.decode(RawLanguage, response.text)
+	if response.status_code == pastemyst.http.Status.ok {
+		return pastemyst.json.decode(RawLanguage, response.text)
 	} else {
 		return error("Error while fetching language details")
 	}	
--- endpoints/paste.v
+++ endpoints/paste.v
@@ -2,10 +2,10 @@ module endpoints
 
 import billyeatcookies.pastemyst
 
-const get_paste_endpoint    = "$main_endpoint/paste/"
-const create_paste_endpoint = "$main_endpoint/paste"
-const delete_paste_endpoint = "$main_endpoint/paste/"
-const edit_paste_endpoint   = "$main_endpoint/paste/"
+const get_paste_endpoint    = "$pastemyst.main_endpoint/paste/"
+const create_paste_endpoint = "$pastemyst.main_endpoint/paste"
+const delete_paste_endpoint = "$pastemyst.main_endpoint/paste/"
+const edit_paste_endpoint   = "$pastemyst.main_endpoint/paste/"
 
 
 pub struct GetPasteConfig {
@@ -14,12 +14,12 @@ pub struct GetPasteConfig {
 }
 
 pub fn get_paste (config GetPasteConfig) ?RawPaste {
-	mut request := http.new_request(.get, get_paste_endpoint + config.id, "") ?
+	mut request := pastemyst.http.new_request(.get, get_paste_endpoint + config.id, "") ?
 	if config.token != "" {
 		request.add_header("Authorization", config.token)
 	}
 	response := request.do() ?
-	if response.status_code != http.Status.not_found {
+	if response.status_code != pastemyst.http.Status.not_found {
 		return json.decode(RawPaste, response.text)
 	} else {
 		println("Paste not found, check the id and token given")
@@ -28,12 +28,12 @@ pub fn get_paste (config GetPasteConfig) ?RawPaste {
 }
 
 pub struct CreatePasteConfig {
-	paste Paste  [required]
+	paste pastemyst.types.paste.Paste  [required]
 	token string
 }
 
 pub fn create_paste (config CreatePasteConfig) ?RawPaste {
-	mut request := http.new_request(.post, create_paste_endpoint, json.encode(config.paste)) ?
+	mut request := pastemyst.http.new_request(.post, create_paste_endpoint, pastemyst.json.encode(config.paste)) ?
 	request.add_header('Content-Type','application/json')
 	
 	if config.token == "" && (config.paste.is_private || config.paste.is_public || config.paste.tags != "") {
@@ -43,7 +43,7 @@ pub fn create_paste (config CreatePasteConfig) ?RawPaste {
 		request.add_header("Authorization", config.token)
 	}
 	response := request.do() ? 
-	return json.decode(RawPaste, response.text)
+	return pastemyst.json.decode(RawPaste, response.text)
 }
 
 pub struct DeletePasteConfig {
@@ -52,29 +52,29 @@ pub struct DeletePasteConfig {
 }
 
 pub fn delete_paste (config DeletePasteConfig) ?bool {
-	mut request := http.new_request(.delete, delete_paste_endpoint + config.id, "") ?
+	mut request := pastemyst.http.new_request(.delete, delete_paste_endpoint + config.id, "") ?
 	if config.token != "" {
 		request.add_header("Authorization", config.token)
 	} else {
 		return error("Token not provided, deletion is an account only feature.")
 	}
 	response := request.do() ?
-	return response.status_code == http.Status.ok
+	return response.status_code == pastemyst.http.Status.ok
 }
 
 pub struct EditPasteConfig {
 	id    string [required]
-	edit  Edit   [required]
+	edit  pastemyst.types.edit.Edit   [required]
 	token string [required]
 }
 
 pub fn edit_paste (config EditPasteConfig) ?RawEdit {
-	mut request := http.new_request(.patch, edit_paste_endpoint + config.id, json.encode(edit)) ?
+	mut request := pastemyst.http.new_request(.patch, edit_paste_endpoint + config.id, pastemyst.json.encode(edit)) ?
 	if config.token != "" {
 		request.add_header("Authorization", config.token)
 	} else {
 		return error("Token not provided, editing is an account only feature.")
 	}
 	response := request.do() ?
-	return json.decode(RawEdit, response.text)
+	return pastemyst.json.decode(RawEdit, response.text)
 }
