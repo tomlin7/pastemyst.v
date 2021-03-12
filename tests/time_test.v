8def5a0f9788e41df833464803b51c227edbfaa0
billyeatcookies <billydevbusiness@gmail.com>
2021-03-12 23:52:50 +0530

    cannot use operator `==` with `billyeatcookies.pastemyst.endpoints.ExpiresInToUnixTimeStampReturnType`

--- tests/time_test.v
+++ tests/time_test.v
@@ -35,7 +35,7 @@ fn test_expires_in_to_unix_timestamp () ? {
 			created_at : preferred_test.created_at,
 			expires_in : preferred_test.expires_in
 		) ?
-		if mut expiry !is bool {
+		if mut expiry is int {
 			assert expiry == preferred_test.expected
 		}
 	}
