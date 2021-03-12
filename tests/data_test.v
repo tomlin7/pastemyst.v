fd768d67dd8696415882fe4552499cf0b7459cdb
billyeatcookies <billydevbusiness@gmail.com>
2021-03-13 00:44:56 +0530

    get_language function returns optional

--- tests/data_test.v
+++ tests/data_test.v
@@ -4,7 +4,7 @@ import billyeatcookies.pastemyst.endpoints
 import billyeatcookies.pastemyst.types
 
 fn test_get_language_by_name() ? {
-    assert endpoints.get_language(name: "unknown language") !is types.RawLanguage
+    assert endpoints.get_language(name: "unknown language") ? !is types.RawLanguage
 
     mut result := endpoints.get_language(name: "python") ?
     if mut result is types.RawLanguage {
@@ -13,7 +13,7 @@ fn test_get_language_by_name() ? {
 }
 
 fn test_get_language_by_extension() ? {
-    assert endpoints.get_language(extension: "unknown extension") !is types.RawLanguage
+    assert endpoints.get_language(extension: "unknown extension") ? !is types.RawLanguage
     
     mut result := endpoints.get_language(extension: "py") ?
     if mut result is types.RawLanguage {
