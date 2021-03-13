da6c6d85d17f9e20d78e06cc3d050a758940607b
billyeatcookies <billydevbusiness@gmail.com>
2021-03-13 12:25:01 +0530

    get_language should return types.RawLanguage

--- endpoints/data.v
+++ endpoints/data.v
@@ -9,14 +9,12 @@ import billyeatcookies.pastemyst.types
 const data_language_endpoint_name           = "$pastemyst.main_endpoint/data/language"
 const data_language_endpoint_extension      = "$pastemyst.main_endpoint/data/languageExt"
 
-type GetLanguageReturnType = types.RawLanguage | bool
-
 pub struct GetLanguageConfig {
 	name      string
 	extension string
 }
 
-pub fn get_language (config GetLanguageConfig) ?GetLanguageReturnType {
+pub fn get_language (config GetLanguageConfig) ?types.RawLanguage {
 	if config.name != "" {
 		mut request := http.new_request(.get, data_language_endpoint_name + "?name=" + config.name, "") ?
 		response := request.do() ?
@@ -24,8 +22,7 @@ pub fn get_language (config GetLanguageConfig) ?GetLanguageReturnType {
 		if response.status_code == int(http.Status.ok) {
 			return json.decode(types.RawLanguage, response.text)
 		} else {
-			println("Error while fetching language details")
-			return false
+			return error("Error while fetching language details")
 		}
 	} else if config.extension != "" {
 		mut request := http.new_request(.get, data_language_endpoint_extension + "?extension=" + config.extension, "") ?
@@ -34,8 +31,7 @@ pub fn get_language (config GetLanguageConfig) ?GetLanguageReturnType {
 		if response.status_code == int(http.Status.ok) {
 			return json.decode(types.RawLanguage, response.text)
 		} else {
-			println("Error while fetching language details")
-			return false
+			return error("Error while fetching language details")
 		}
 	}
 		
